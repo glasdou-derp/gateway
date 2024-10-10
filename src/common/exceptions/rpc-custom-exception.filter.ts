c41ea3e5d458bb6a6816a7a75d05ca0704d924c4
glasdou.dev <dr.glasdou@gmail.com>
2024-10-10 16:11:56 -0600

    Refactor RpcCustomExceptionFilter to handle empty responses and return appropriate error messages

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,32 +1,24 @@
-import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { ArgumentsHost, Catch, RpcExceptionFilter } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Catch(RpcException)
 export class RpcCustomExceptionFilter implements RpcExceptionFilter<RpcException> {
   catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-    host.switchToRpc(); // Handles the RPC context, relevant for NATS too
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
     const rpcError = exception.getError();
     const rpcErrorString = rpcError.toString();
 
-    if (rpcErrorString.includes('Empty response')) {
-      return throwError(() => ({
-        status: 500,
-        message: rpcErrorString.substring(0, rpcErrorString.indexOf('(') - 1),
-      }));
-    }
+    if (rpcErrorString.includes('Empty response'))
+      return response.status(500).json({ status: 500, message: rpcErrorString.substring(0, rpcErrorString.indexOf('(') - 1) });
 
     if (typeof rpcError === 'object' && 'status' in rpcError && 'message' in rpcError) {
       const status = isNaN(+rpcError.status) ? 400 : rpcError.status;
-      return throwError(() => ({
-        status: status,
-        message: rpcError.message,
-      }));
+      return response.status(status).json(rpcError);
     }
 
-    return throwError(() => ({
-      status: 400,
-      message: rpcError,
-    }));
+    return response.status(400).json({ status: 400, message: rpcError });
   }
 }
